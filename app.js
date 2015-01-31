8edd2814fb178021315711714a5fe349f79ca8be
Philippe Richard <philippe.richard93@gmail.com>
2015-01-31 00:36:16 -0500

    Removed mongoose database

--- app.js
+++ app.js
@@ -1,18 +1,8 @@
 var express = require('express'),
   config = require('./config/config'),
-  glob = require('glob'),
-  mongoose = require('mongoose');
+  glob = require('glob');
 
-mongoose.connect(config.db);
-var db = mongoose.connection;
-db.on('error', function () {
-  throw new Error('unable to connect to database at ' + config.db);
-});
 
-var models = glob.sync(config.root + '/app/models/*.js');
-models.forEach(function (model) {
-  require(model);
-});
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
